af3c982c5fad43cc76608ab1b08bd3426a9ba451
Babault <nathan.babault@epitech.eu>
2026-03-16 02:20:38 +0000

    correction

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -1,12 +1,12 @@
 // src/app/api/checkout/route.js
 import { NextResponse } from 'next/server';
-import connectDB from '@/lib/db'; // Votre fonction de connexion
+import dbConnect from '@/dbConnect'; // Votre fonction de connexion
 import Product from '@/models/Product';
 import Order from '@/models/Order';
 
 export async function POST(req) {
   try {
-    await connectDB();
+    await dbConnect();
     const { items, customerDetails } = await req.json();
 
     // 1. Vérifier et mettre à jour le stock pour chaque article
